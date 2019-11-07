f7fe79b0af2a8fa7953a9295e4eb6ebe0bbb74ba
AnaYaeR <42159669+AnaYaeR@users.noreply.github.com>
2019-11-07 16:12:22 -0500

    Add files via upload

---
+++ class/Audio.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Audio;
+
+namespace xd
+{
+	class Audio
+	{
+		public Audio(string filename)
+		{
+
+			Bitrate = 256;
+			Filename = filename;
+		}
+
+		public override string ToString()
+		{
+			return $"{Filename} @{Bitrate}kbps";
+		}
+
+		~Audio()
+		{
+		}
+
+		private SFML.Audio.Music Src;
+		private string Filename;
+		private uint Bitrate;
+	}
+}
---
+++ class/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace xd
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Audio music = new Audio("xd.mp3");
+
+			Console.WriteLine(music.ToString());
+			Console.ReadLine();
+		}
+	}
+}
